64839dcdcd7d2de1d5a9bbc594821e96bddcd48a
Yehezkiel Petra <Yehezkielpetra.ypk@gmail.com>
2026-04-20 17:51:42 +0700

    project 2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
 import ComingSoon from "../assets/img/ComingSoon.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
@@ -16,9 +17,9 @@ export const Projects = () => {
       imgUrl: projImg1,
     },
     {
-      title: "Coming Soon",
-      description: "Stay Tuned",
-      imgUrl: ComingSoon,
+      title: "AI-Expense Tracker",
+      description: "Artificial Intelligence",
+      imgUrl: projImg2,
     },
     {
       title: "Coming Soon",
